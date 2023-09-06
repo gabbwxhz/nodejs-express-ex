00c21fa78cd8eef3099f3f357099dae5d59e9fab
gabriel <vnvsgabe@gmail.com>
2023-09-06 16:45:04 -0300

    criado rotas

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,27 @@
-const express = require('express')
-const app = express()
+import * as express from 'express'
+
+const app = express() 
+
+
+// criação de rotas
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('GET /')
 })
 
-app.get('/teste', (req, res) => {
-    res.send('Hello World, teste')
+app.post('/', (req, res) => {
+    res.status(201).send('POST /') // 201: codigo relacionado a criar alguma coisa
+})
+
+app.put('/', (req, res) => {
+    res.send('PUT /')
+})
+app.delete('/', (req, res) => {
+    res.send('DELETE /')
+})
+
+app.patch('/', (req, res) => {
+    res.send('PATCH /')
 })
 
 app.listen(3000, () => console.log(`online - http://localhost:3000`))
