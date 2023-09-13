9a865ceeea829712d1cb634c0d8de0736a6ce485
gabriel <vnvsgabe@gmail.com>
2023-09-13 20:56:51 -0300

    criado service de posts

--- src/modules/post/postController.js
+++ src/modules/post/postController.js
@@ -1,15 +1,30 @@
 import { Router } from "express"
 import { verifyAccesToken } from "../../utils/auth"
 
+import { createPost, getPosts } from "./postService"
+
 const router = Router()
 
 router.post('/', verifyAccesToken, (req, res) => {
-    res.send('CREATE POST /')
+    try {
+        const post = createPost(req.body, req.user)
+        res.status(200).send(post)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 })
 
 router.get('/:id?', verifyAccesToken, (req, res) => { // so acontece quando existir a variavel
     // duas opções: listar todos ou apenas um
-    res.send('CREATE POST /')
+    try {
+        const posts = getPosts(req.params.id)
+        res.status(200).send(posts)
+    } catch (err) {
+        if (err.message === 'post_nao_existe')
+            return res.status(400).send(err.message)
+
+        return res.status(500).send(err.message)
+    }
 })
 
 
